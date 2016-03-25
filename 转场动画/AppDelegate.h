915a3217c0753cac140266e9e69de3977f99c5ca
wangaiguo <wangaiguo@wangaiguodeMac-mini.local>
2016-03-25 17:14:41 +0800

    Initial Commit

---
+++ 转场动画/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  转场动画
+//
+//  Created by wangaiguo on 16/3/25.
+//  Copyright © 2016年 wangaiguo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 转场动画/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  转场动画
+//
+//  Created by wangaiguo on 16/3/25.
+//  Copyright © 2016年 wangaiguo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
